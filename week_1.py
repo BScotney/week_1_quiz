d8d41113f15235b322ab9bf22c01f3d49f232de6
Brendan Scotney <scotneb77@gmail.com>
2022-07-13 17:13:22 +1000

    Initial Commit

---
+++ week_1.py
@@ -0,0 +1,12 @@
+# print name
+print("Brendan")
+
+# calculate fav number squared
+fav_number = 3
+fav_squared = fav_number**2
+
+# count up to fav number squared while printing
+counter = 1
+while counter <= fav_squared:
+    print(counter)
+    counter += 1
